bd66c9cca10bb56a4969984671f4cbbc6bc5d642
joonjoon0425 <joonjoon0425@gmail.com>
2026-06-20 16:12:23 +0900

    Renamed refactored components and deleted expired components. close #5

--- include/gridworld/gridworld.hpp
+++ include/gridworld/gridworld.hpp
@@ -1,44 +1,58 @@
 #ifndef _GRIDWORLD_HPP_
 #define _GRIDWORLD_HPP_
 
-#include <env.hpp>
-#include <point.hpp>
-#include <set>
+#include "core/env.hpp"
 
-namespace gridworld_2D{
-    constexpr size_t ACTION_NUM = 4;
-    using grid_state = point;
+namespace gridworld {
+    struct point2D {
+        int x_;
+        int y_;
 
-    enum class grid_action: int {up, down, left, right};
+        bool operator==(const point2D& point) const {
+            return x_ == point.x_ && y_ == point.y_;
+        }
+    };
 
-    class grid_env : public environment<grid_state, grid_action, grid_env> {
-        grid_state cur_;
-        grid_state start_;
-        grid_state goal_;
-        grid_state trap_;
+    enum class direction2D : int {UP, DOWN, LEFT, RIGHT};
 
+    class env_grid2D : public environment<point2D, direction2D, env_grid2D> {
+    public:
+        env_grid2D(int width, int height, state_t start, state_t goal, state_t trap);
+
+        state_t reset(bool random) override;
+        std::tuple<state_t, float, bool> step(const state_t&, const action_t&) override;
+
+    private:
         int width_;
         int height_;
         
-    public:
-        grid_env(int width, int height, grid_state start, grid_state goal, grid_state trap);
-        void reset(bool random = false);
-        std::tuple<grid_state, float, bool> step(grid_action);
-        
-        grid_state cur() const {return cur_;}
-        grid_state trap() const {return trap_;}
-        grid_state goal() const {return goal_;}
+        state_t start_;
+        state_t goal_;
+        state_t trap_;
 
+    public:
         int width() const {return width_;}
         int height() const {return height_;}
 
-        static std::set<grid_env::action_t> impl_get_possible_actions(const grid_env::state_t& state) {
-            std::set<grid_env::action_t> actions{grid_action::up, grid_action::down, grid_action::left, grid_action::right};
-            if (state.x_ == state.max_x_ - 1) actions.erase(grid_action::right);
-            else if (state.x_ == 0) actions.erase(grid_action::left);
+        state_t trap() const {return trap_;}
+        state_t goal() const {return goal_;}
+        state_t start() const {return start_;}
+
+        int impl_state_to_index(const state_t& state) const {
+            return state.y_ * width_ + state.x_;
+        }
+
+        int impl_action_to_index(const action_t& action) const {
+            return static_cast<int>(action);
+        }
+
+        std::set<action_t> impl_get_possible_actions(const state_t& state) const {
+            std::set<action_t> actions{action_t::UP, action_t::DOWN, action_t::LEFT, action_t::RIGHT};
+            if (state.x_ == width_ - 1) actions.erase(action_t::RIGHT);
+            else if (state.x_ == 0) actions.erase(action_t::LEFT);
             
-            if (state.y_ == state.max_y_ - 1) actions.erase(grid_action::up);
-            else if (state.y_ == 0) actions.erase(grid_action::down);
+            if (state.y_ == height_ - 1) actions.erase(action_t::UP);
+            else if (state.y_ == 0) actions.erase(action_t::DOWN);
 
             return actions;
         }
